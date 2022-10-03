249a3362f3e2910d924dbcb9d078f91e66def74a
ChrisIOET <christian.scarpati@ioet.com>
2022-10-03 15:20:28 -0300

    first tests login page

--- src/auth/components/login-page/loginPage.jsx
+++ src/auth/components/login-page/loginPage.jsx
@@ -1,8 +1,15 @@
+import { TextField, Button } from "@material-ui/core"
+
 export const LoginPage = () => {
 
 
     return (
-        <h1>Login</h1>
+        <>
+            <h1>login</h1>
+            <TextField label="email" id="email" />
+            <TextField label="password" id="password" type="passwword" />
+            <Button>send</Button>
+        </>
     )
 }
 
--- src/auth/components/login-page/loginPage.test.jsx
+++ src/auth/components/login-page/loginPage.test.jsx
@@ -3,11 +3,18 @@ import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react"
 import { LoginPage } from "./loginPage"
 
+beforeEach(() => render(<LoginPage />))
 
 describe("When login page is mounted", () => {
+
     it("must display the login word", () => {
-        render(<LoginPage />)
-        
         expect(screen.getByText(/Login/i)).toBeInTheDocument()
     })
+
+    it("must display a form with email, password and a submit button", () => {
+        expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
+        expect(screen.getByRole('button', { name: /send/i }))
+    })
+
 })
