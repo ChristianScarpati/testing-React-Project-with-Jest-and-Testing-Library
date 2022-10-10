db8d8a4f17fb73c5a68077c75ec37d96f28502de
ChrisIOET <christian.scarpati@ioet.com>
2022-10-09 22:15:13 -0300

    finish start project

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,46 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    // setupFiles: ['./jest.setup.js'],
-    transformIgnorePatterns: [],
-
-    // ModuleNameMapper sólo si ocupamos importar CSS en nuestros componentes para el testing
+    roots: ["<rootDir>/src"],
+    collectCoverageFrom: [
+      "src/**/*.{js,jsx,ts,tsx}",
+      "!src/**/*.d.ts",
+      "!src/mocks/**",
+    ],
+    coveragePathIgnorePatterns: [],
+    // setupFilesAfterEnv: ["./config/jest/setupTests.js"],
+    testEnvironment: "jsdom",
+    modulePaths: ["<rootDir>/src"],
+    // transform: {
+      // "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
+      // "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+      // "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)":
+      //   "<rootDir>/config/jest/fileTransform.js",
+    // },
+    transformIgnorePatterns: [
+      "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+      "^.+\\.module\\.(css|sass|scss)$",
+    ],
+    modulePaths: ["<rootDir>/src"],
     moduleNameMapper: {
-        '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
+      "^react-native$": "react-native-web",
+      "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
     },
-   
-}
+    moduleFileExtensions: [
+      // Place tsx and ts to beginning as suggestion from Jest team
+      // https://jestjs.io/docs/configuration#modulefileextensions-arraystring
+      "tsx",
+      "ts",
+      "web.js",
+      "js",
+      "web.ts",
+      "web.tsx",
+      "json",
+      "web.jsx",
+      "jsx",
+      "node",
+    ],
+    // watchPlugins: [
+    //   "jest-watch-typeahead/filename",
+    //   "jest-watch-typeahead/testname",
+    // ],
+    resetMocks: true,
+  };
